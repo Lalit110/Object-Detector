e22288bca6e0d02bdb130e31e782aedec5f49dcf
Lalit <83270963+Lalit110@users.noreply.github.com>
2022-10-29 18:54:02 +0530

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage("kitchen.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+}
+
+function modelLoaded(){
+    console.log("Model Is Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
